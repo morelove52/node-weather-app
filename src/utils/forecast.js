e4342475a786ba74d2c0010dc8692f4d649595bb
Mohamed Atia <mh.mohamed@live.com>
2020-05-23 01:05:55 +0200

    update error

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,21 +7,24 @@ const forecast = (latitude, longitude, callback) => {
     request({
         url,
         json: true
-    }, (error, { body }) => {
+    }, (error, {
+        body
+    }) => {
         if (error) {
             callback('No internet connection', undefined)
         } else if (body.error) {
             callback(body.error.message, undefined)
         } else {
             callback(undefined, `Today:
-            -It's currently ${body.current.condition.text}.
-            -Now is ${body.current.temp_c}\u2103.
-            -It feels like ${body.current.feelslike_c}\u2103.
+            -It's currently ${body.current.condition.text}
+            -Now is ${body.current.temp_c}\u2103
+            -It feels like ${body.current.feelslike_c}\u2103
             Tomorrow:
-            -Max: ${body.forecast.forecastday[1].day.maxtemp_c}\u2103.
-            -Min: ${body.forecast.forecastday[1].day.mintemp_c}\u2103`.
+            -Max: ${body.forecast.forecastday[1].day.maxtemp_c}\u2103
+            -Min: ${body.forecast.forecastday[1].day.mintemp_c}\u2103`)
         }
     })
 }
 
 module.exports = forecast
+
